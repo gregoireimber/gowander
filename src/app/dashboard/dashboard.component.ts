5aee3d26a9874a2b43722c891edb9c89310eb564
Gregoire Imber <gregoire.i@bluefusesystems.com>
2021-12-23 16:59:30 +0000

    link authentication to the db

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -14,15 +14,14 @@ export class DashboardComponent implements OnInit {
   constructor(private router: Router, private authService: AuthService, public dialog: MatDialog) { }
 
   public isLoggedIn = false;
+  public userInformation: any = '';
 
   ngOnInit(): void {
     this.isLoggedIn = this.authService.isLoggedIn;
+    this.getUserInfo();
 
     // If not logged in, navigate to log in
     if (this.isLoggedIn === false) this.router.navigateByUrl('/login');
-
-    // Get the user information
-    // Will only be able to do this when I link to the database and have the users populating it with all their information
   }
 
   public onAdd(): void {
@@ -34,6 +33,13 @@ export class DashboardComponent implements OnInit {
     //   console.log('The dialog was closed');
     //   this.animal = result;
     // });
+  }
+
+  public async getUserInfo() {
+    const userId = this.authService.getUserId();
+    this.userInformation = await this.authService.getUserInformation(userId).then((response: any) => {
+      return response;
+    });
   }
 }
 
